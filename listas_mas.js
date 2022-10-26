23a547b50b6f16d667c558e341f115a075769ebd
Maria Cruz Tubio <mctubio@gmail.com>
2022-10-26 19:35:55 -0300

    ejercicio_2

---
+++ listas_mas.js
@@ -0,0 +1,26 @@
+var vaiable;
+let variableLet;
+
+var a = 1;
+console.log(a);
+
+a=4;
+console.log(a);
+
+console.log(typeof'a')
+
+const lista = [1,'a', false];
+const lista2 = new Array(2,'a', false );
+const lista3 =new Array(4);
+lista3 [0]='hola';
+lista3 [1]='soy';
+lista3[2]='una';
+lista3[3]='lista'
+
+console.log(lista)
+console.log(lista2)
+console.log(lista3)
+
+const ahora = new Date('2020, , 5');
+
+console.log(ahora);
